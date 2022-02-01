d808dee92b86744918821b7f94d3954e43cecd5c
Akash sarkar <theakashsarkar7@gmail.com>
2022-02-01 17:26:11 +0600

    psd to html

---
+++ js/active.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        navText:['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+})
