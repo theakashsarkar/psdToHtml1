9f594f60f36792585a11ecf89485cce8c005f89c
Akash sarkar <theakashsarkar7@gmail.com>
2022-02-02 00:46:04 +0600

    project part

--- js/active.js
+++ js/active.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
+    $('.slider-active').owlCarousel({
         loop:true,
-        margin:10,
-        nav:true,
+        nav:false,
         navText:['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
         responsive:{
             0:{
@@ -13,6 +12,22 @@ $(document).ready(function(){
             },
             1000:{
                 items:1
+            }
+        }
+    })
+    $('.project-active').owlCarousel({
+        loop:true,
+        nav:true,
+        navText:['<i class="fas fa-angle-left"></i>','<i class="fas fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:4
             }
         }
     })
